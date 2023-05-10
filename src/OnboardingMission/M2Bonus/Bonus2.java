0d46cbc3401d8c90ac39641706e5bdd3fb1da61d
nayonsoso <panda0329@naver.com>
2023-05-10 16:41:28 +0900

    230510

---
+++ src/OnboardingMission/M2Bonus/Bonus2.java
@@ -0,0 +1,19 @@
+// 연속된 숫자를 제거하는 문제
+// 프로그래머스 12906
+
+package OnboardingMission.M2Bonus;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Bonus2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrlist = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+//        arrlist.add(1);
+//        arrlist.add(2);
+//        arrlist.add(3);
+//        arrlist.add(4);
+//        arrlist.add(5);
+        System.out.println(arrlist);
+    }
+}
